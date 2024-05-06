3a72e17e6b65ebbdde22b0cb8dccdea608e2174a
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2024-05-06 22:40:09 +0300

    один

--- pointers.cpp
+++ pointers.cpp
@@ -1,8 +1,14 @@
 ﻿// тема указатели
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "rus");
+	int age = 30;
+	const double Pi = 3.1416;
+	// использование & для поиска адресса впамяти 
+	cout << "Адрес age: 0x " << hex << &age << endl;
+	cout << "Адрес Pi:  0x " << hex << &Pi << endl;
+	return 0;
+
 }
